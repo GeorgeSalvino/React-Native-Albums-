5b187b83d94636d3b08e4ee4c192d1f01b235de4
George Salvino <georgesalvino98@gmail.com>
2019-06-01 01:13:34 -0300

    Initial Commit
    At this point, the app only have a Header and displays the album titles in a list

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import a libray to help crat a content
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+//Create a component
+const App = () => (
+        <View>
+            <Header headerText={'Albums'} />
+            <AlbumList />
+        </View>
+);
+
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
